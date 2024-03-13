218fb6ae57a04d074fa8330c4a99ff3918202772
Farid Domat <fariddomat.000@gmail.com>
2024-03-13 19:13:47 +0300

    fixes

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,95 +1,188 @@
 @extends('layouts.site')
 @section('title', trans('site.blogs'))
 @section('styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
     <style>
-        .de-navbar-left.de_light #subheader {
-            background-size: cover !important;
+        #customers-testimonials.owl-carousel .owl-dots .owl-dot span {
+            background: #DF1F26 !important;
+            margin-bottom: 25px !important;
         }
 
-        #subheader {
-            background-size: cover !important;
+        .shadow-effect {
+            background: #fff;
+            padding: 20px;
+            border-radius: 4px;
+            text-align: center;
+            border: 1px solid #ECECEC;
+            box-shadow: 0 19px 38px rgba(0, 0, 0, 0.10), 0 15px 12px rgba(0, 0, 0, 0.02);
         }
 
-        .stitle {
-            font-weight: bold;
-            font-size: 20px !important;
+        #customers-testimonials .shadow-effect p {
+            font-family: inherit;
+            font-size: 17px;
+            line-height: 1.5;
+            margin: 0 0 17px 0;
+            font-weight: 300;
         }
 
-        @media screen and (max-width: 460px) {
-            #section-content {
-                padding: 35px 25px 70px;
-            }
+        .testimonial-name {
+            margin: -17px auto 0;
+            display: table;
+            width: auto;
+            background: #3190E7;
+            padding: 9px 35px;
+            border-radius: 12px;
+            text-align: center;
+            color: #fff;
+            box-shadow: 0 9px 18px rgba(0, 0, 0, 0.12), 0 5px 7px rgba(0, 0, 0, 0.05);
+        }
 
+        #customers-testimonials .item {
+            text-align: center;
+            padding: 50px;
+            margin-bottom: 80px;
+            opacity: .2;
+            transform: scale3d(0.8, 0.8, 1);
+            transition: all 0.3s ease-in-out;
+        }
 
-            .blog-item {
-                margin-top: 35px;
-            }
+        #customers-testimonials .owl-item.active.center .item {
+            opacity: 1;
+            transform: scale3d(1.0, 1.0, 1);
+        }
 
-            h3,
-            p {
-                height: auto !important;
-            }
+        .owl-carousel .owl-item img {
+            transform-style: preserve-3d;
+            max-width: 90px;
+            margin: 0 auto 17px;
+        }
 
+        #customers-testimonials.owl-carousel .owl-dots .owl-dot.active span,
+        #customers-testimonials.owl-carousel .owl-dots .owl-dot:hover span {
+            background: #3190E7;
+            transform: translate3d(0px, -50%, 0px) scale(0.7);
+        }
+
+        #customers-testimonials.owl-carousel .owl-dots {
+            display: inline-block;
+            width: 100%;
+            text-align: center;
+        }
+
+        #customers-testimonials.owl-carousel .owl-dots .owl-dot {
+            display: inline-block;
+        }
+
+        #customers-testimonials.owl-carousel .owl-dots .owl-dot span {
+            background: #3190E7;
+            display: inline-block;
+            height: 20px;
+            margin: 0 2px 5px;
+            transform: translate3d(0px, -50%, 0px) scale(0.3);
+            transform-origin: 50% 50% 0;
+            transition: all 250ms ease-out 0s;
+            width: 20px;
         }
     </style>
+@endsection
+@section('scripts')
+    <script>
+        jQuery(document).ready(function($) {
+            "use strict";
+            //  TESTIMONIALS CAROUSEL HOOK
+            $('#customers-testimonials').owlCarousel({
+                loop: true,
+                center: true,
+                items: 3,
+                margin: 0,
+                autoplay: true,
+                dots: true,
+                autoplayTimeout: 2500,
+                smartSpeed: 450,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    768: {
+                        items: 2
+                    },
+                    1170: {
+                        items: 3
+                    }
+                }
+            });
+        });
+    </script>
 @endsection
 @section('content')
 
     <!-- Page title -->
-    <section class="page-title-wrap position-relative bg-light"  data-bg-img="{{ asset($info->blog_header_image) }}">
+    <section class="page-title-wrap position-relative bg-light" data-bg-img="{{ asset($info->service_image) }}">
         <div id="particles_js"></div>
         <div class="container">
             <div class="row">
-                <div class="col-11">
+                <div class="col-12">
                     <div class="page-title position-relative pt-5 pb-5">
                         <ul class="custom-breadcrumb roboto list-unstyled mb-0 clearfix" data-animate="fadeInUp"
                             data-delay="1.2">
                             <li><a href="{{ route('home') }}">@lang('site.home')</a></li>
                             <li><i class="fas fa-angle-double-right"></i></li>
-                            <li><a href="#">@lang('site.blogs')</a></li>
+                            <li><a href="#">@lang('site.services')</a></li>
                         </ul>
-                        <h1 data-animate="fadeInUp" data-delay="1.3">@lang('site.blogs')</h1>
+                        <h1 data-animate="fadeInUp" data-delay="1.3">@lang('site.services')</h1>
                     </div>
                 </div>
+
             </div>
         </div>
     </section>
     <!-- End of Banner -->
 
-    <!-- blog -->
-    <section class="blog pt-7 pb-7">
+
+    <!-- Service -->
+    <section class="testimonials blog" dir="ltr">
         <div class="container">
-            <!-- Posts -->
+
             <div class="row">
-                @foreach ($blogs as $index => $blog)
-                    <div class="col-lg-4 col-md-6">
-                        <div class="single-post" data-animate="fadeInUp" data-delay="{{ 1.5 + $index / 10 }}">
-                            <div class="image-hover-wrap">
-                                <img src="{{ asset($blog->image) }}" alt="">
-                                <div
-                                    class="image-hover-content d-flex justify-content-center align-items-center text-center">
-                                    <ul class="list-inline">
-                                        <li><a href="{{ route('blog', $blog->slug) }}"><i class="fas fa-link"></i></a></li>
-                                        {{-- <li><a href="#"><i class="fas fa-share-alt"></i></a></li> --}}
-                                    </ul>
+                <div class="col-sm-12">
+                    <div id="customers-testimonials" class="owl-carousel " data-animate="fadeInUp" data-delay="1.5">
+
+                        @foreach ($blogs as $index => $blog)
+                            <!--TESTIMONIAL 1 -->
+                            <div class="item">
+                                <div class="shadow-effect">
+                                    <div class="single-post" data-animate="">
+                                        <div class="image-hover-wrap">
+                                            <img src="{{ asset($blog->image) }}" alt="">
+                                            <div
+                                                class="image-hover-content d-flex justify-content-center align-items-center text-center">
+                                                <ul class="list-inline">
+                                                    <li><a href="{{ route('blog', $blog->slug) }}"><i class="fas fa-link"></i></a></li>
+                                                    {{-- <li><a href="#"><i class="fas fa-share-alt"></i></a></li> --}}
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <span> {{ $blog->updated_at->format('d F Y') }}</span>
+                                        <h3>{{ $blog->title }}</h3>
+                                        @php
+                                            $desc = Str::limit(strip_tags($blog->description), 180, ' ...');
+                                        @endphp
+                                        <h4>{{ $desc }}</h4>
+
+                                    </div>
+
+                                </div>
+                                <div class="testimonial-name" style="background-color: #DF1F26">
+                                    <a href="{{ route('blog', $blog->slug) }}"
+                                        class="btn btn-secondary">@lang('site.read_more')<i class="fas fa-caret-right"></i></a>
                                 </div>
                             </div>
-                            <span> {{ $blog->updated_at->format('d F Y') }}</span>
-                            <h3>{{ $blog->title }}</h3>
-                            @php
-                                $desc = Str::limit(strip_tags($blog->description), 300, ' ...');
-                            @endphp
-                            <h4>{{ $desc }}</h4>
-                            <a href="{{ route('blog', $blog->slug) }}">@lang('site.read_more')<i
-                                    class="fas fa-caret-right"></i></a>
-                        </div>
+                            <!--END OF TESTIMONIAL 1 -->
+                        @endforeach
                     </div>
-                @endforeach
-
+                </div>
             </div>
-
-            <!-- Pagination -->
-
         </div>
     </section>
     <!-- End of Service -->
