4ba061a3d23632a891d9d8efef928f4f638d9e2b
Farid Domat <fariddomat.000@gmail.com>
2024-03-14 18:53:50 +0300

    fixes

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -236,6 +236,121 @@
     </script>
 @endsection
 
+@section('styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+    <style>
+        #customers-testimonials.owl-carousel .owl-dots .owl-dot span {
+            background: #DF1F26 !important;
+            margin-bottom: 25px !important;
+        }
+
+        .shadow-effect {
+            background: #fff;
+            padding: 20px;
+            border-radius: 4px;
+            text-align: center;
+            border: 1px solid #ECECEC;
+            box-shadow: 0 19px 38px rgba(0, 0, 0, 0.10), 0 15px 12px rgba(0, 0, 0, 0.02);
+        }
+
+        #customers-testimonials .shadow-effect p {
+            font-family: inherit;
+            font-size: 17px;
+            line-height: 1.5;
+            margin: 0 0 17px 0;
+            font-weight: 300;
+        }
+
+        .testimonial-name {
+            margin: -17px auto 0;
+            display: table;
+            width: auto;
+            background: #3190E7;
+            padding: 9px 35px;
+            border-radius: 12px;
+            text-align: center;
+            color: #fff;
+            box-shadow: 0 9px 18px rgba(0, 0, 0, 0.12), 0 5px 7px rgba(0, 0, 0, 0.05);
+        }
+
+        #customers-testimonials .item {
+            text-align: center;
+            padding: 50px;
+            margin-bottom: 80px;
+            opacity: .2;
+            transform: scale3d(0.8, 0.8, 1);
+            transition: all 0.3s ease-in-out;
+        }
+
+        #customers-testimonials .owl-item.active.center .item {
+            opacity: 1;
+            transform: scale3d(1.0, 1.0, 1);
+        }
+
+        .owl-carousel .owl-item img {
+            transform-style: preserve-3d;
+            max-width: 90px;
+            margin: 0 auto 17px;
+        }
+
+        #customers-testimonials.owl-carousel .owl-dots .owl-dot.active span,
+        #customers-testimonials.owl-carousel .owl-dots .owl-dot:hover span {
+            background: #3190E7;
+            transform: translate3d(0px, -50%, 0px) scale(0.7);
+        }
+
+        #customers-testimonials.owl-carousel .owl-dots {
+            display: inline-block;
+            width: 100%;
+            text-align: center;
+        }
+
+        #customers-testimonials.owl-carousel .owl-dots .owl-dot {
+            display: inline-block;
+        }
+
+        #customers-testimonials.owl-carousel .owl-dots .owl-dot span {
+            background: #3190E7;
+            display: inline-block;
+            height: 20px;
+            margin: 0 2px 5px;
+            transform: translate3d(0px, -50%, 0px) scale(0.3);
+            transform-origin: 50% 50% 0;
+            transition: all 250ms ease-out 0s;
+            width: 20px;
+        }
+    </style>
+@endsection
+@section('scripts')
+    <script>
+        jQuery(document).ready(function($) {
+            "use strict";
+            //  TESTIMONIALS CAROUSEL HOOK
+            $('#customers-testimonials').owlCarousel({
+                loop: true,
+                center: true,
+                items: 3,
+                margin: 0,
+                autoplay: true,
+                dots: true,
+                autoplayTimeout: 2500,
+                smartSpeed: 450,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    768: {
+                        items: 2
+                    },
+                    1170: {
+                        items: 3
+                    }
+                }
+            });
+        });
+    </script>
+@endsection
 @section('content')
 
     <!-- Page title -->
@@ -263,7 +378,6 @@
     <!-- End of Banner -->
 
 
-    <div id="content" class="nopadding">
         <section id="section-content">
             <div class="container">
                 <div class="row">
@@ -395,7 +509,48 @@
         </section>
         <!-- logo carousel section close -->
 
+        <section class="testimonials blog" dir="ltr">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div id="customers-testimonials" class="owl-carousel " data-animate="fadeInUp" data-delay="1.5">
+                                <h2 class="text-center">الخدمات الفرعية</h2>
+                            @foreach ($service->subServices as $index => $service)
+                                <!--TESTIMONIAL 1 -->
+                                <div class="item">
+                                    <div class="shadow-effect">
+                                        <div class="single-post" data-animate="">
+                                            <div class="image-hover-wrap">
+                                                <img class="img-fluid" src="{{ asset($service->image) }}" alt="">
+                                                <div
+                                                    class="image-hover-content d-flex justify-content-center align-items-center text-center">
+                                                    <ul class="list-inline">
+                                                        <li><a href="{{ route('service', $service->slug) }}"><i
+                                                                    class="fas fa-link"></i></a>
+                                                        </li>
+                                                        {{-- <li><a href="#"><i class="fas fa-share-alt"></i></a></li> --}}
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                            <h3>{{ $service->title }}</h3>
+                                            <h4>{{ Str::limit($service->brief, 140) }}</h4>
+
+                                        </div>
+
+                                    </div>
+                                    <div class="testimonial-name" style="background-color: #DF1F26">
+                                        <a href="{{ route('service', $service->slug) }}"
+                                            class="btn btn-secondary">@lang('site.read_more')<i class="fas fa-caret-right"></i></a>
+                                    </div>
+                                </div>
+                                <!--END OF TESTIMONIAL 1 -->
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
 
-    </div>
 @endsection
